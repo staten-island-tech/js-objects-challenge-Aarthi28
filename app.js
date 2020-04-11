12da27d0549a26bb9b5443699341535e163dddf5
Aarthi <aarthi.hingorani@sitechhs.com>
2020-04-11 13:10:12 -0400

    4/8/20

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ Angel.name = "Angel";
 /* const Angel = {};
 Angel.dob = Riya.dob; */
 
-const presidents = [
+/* const presidents = [
     { name: "Donald Trump", terms: 1, party: "R" },
     { name: "Barack Obama", terms: 2, party: "D" },
     { name: "George W. Bush", terms: 2, party: "R" },
@@ -92,7 +92,48 @@ const presidents = [
   console.log(Last3);
 
   //4) log all dems who served 2 terms. HINT use chain filter, filter and slice
-  const Dems2 = presidents.filter(president => president.party === "D").filter(president => president.terms === 2).slice(/*???*/);
-  console.log(Dems2);
+  const Dems2 = presidents.filter(president => president.party === "D").filter(president => president.terms === 2).slice(/*???*///);
+  //console.log(Dems2);
 
-  //BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done"
+  //BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done" */
+
+  ////////////////////////////////////////////////////////////////
+//const header = document.querySelector(".header");
+/* const items = document.querySelectorAll(".list-item");
+const itemsArr = Array.from(items);
+itemsArr.forEach(element=>element.style.backgroundColor="red"); */
+
+//const header = document.querySelector("#Idheader");
+/* const header = document.getElementById("Idheader");
+const toggle = document.querySelector(".toggle"); */
+/* header.classList.add("Karen"); //adds and removes classes easily
+header.classList.remove("Karen");   */
+/* toggle.addEventListener("click", function(){
+    header.classList.toggle("Karen");
+}); */
+
+//button clicked, change background to red
+//button clicked, change background to blue 
+const background = document.querySelector(".background");
+const button = document.querySelector(".button");
+const headerTwo = document.querySelector(".header-two");
+
+/* button.addEventListener("click", function(){
+    if(background.style.backgroundColor !== "rgb(255, 0, 0)"{
+        background.style.backgroundColor = "rgb(255, 0, 0)";
+    } else{
+        background.style.backgroundColor = "rgb(0, 0, 255)";
+        headerTwo.textContent = "Blue";
+    }
+}); */
+
+//generate 3 numbers for R G and B (store in variables)
+//get random number, make random number usuable
+
+button.addEventListener("click", function(){
+    const R = Math.floor((Math.random())*255); //scoped variable
+    const G = Math.floor((Math.random())*255);
+    const B = Math.floor((Math.random())*255);
+    background.style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
+    headerTwo.textContent =`rgb(${R}, ${G}, ${B})`;
+});
